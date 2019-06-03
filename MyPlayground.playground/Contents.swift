ef22411dcfb8d14e5afbeb309edf8b5f854cbaa7
coastchb <hbcao@mobvoi.com>
2019-06-03 21:03:20 +0800

    initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+var i = 2       // a variable
+let a = 1 + 2   // a const
+i = 3
+//a = 4
+
+
+for x in 0...10{
+    print("\(x) ", terminator: "")
+}
+print("")
+
+
+var j : Int = 3   // assign data type explicitly
+//j = "haha"        // cannot assign "String" value to "Int" variable
+
+// how to get input from user?
+//let theInput = readLine()
+
+var name = "Apple"
+var site = "apple.com"
+print("\(name) official site is:\(site)")
