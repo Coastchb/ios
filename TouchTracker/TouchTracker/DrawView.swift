742743a94afedd908d229db4562b419d177cd657
Coastchb <Coastchb@sina.com>
2019-08-13 09:29:30 +0800

    TouchTracker modified

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -13,6 +13,15 @@ class DrawView: UIView{
     //var currentLine: Line?
     var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
+    var selectedLineIndex : Int? {
+        didSet {
+            if selectedLineIndex == nil {
+                let menu = UIMenuController.shared
+                menu.setMenuVisible(false, animated: true)
+            }
+        }
+    }
+    var moveRecognizer: UIPanGestureRecognizer!
     
     @IBInspectable var finishedLineColor: UIColor =
         UIColor.black {
@@ -65,6 +74,13 @@ class DrawView: UIView{
         for (_,line) in currentLines{
             stoke(line)
         }
+        
+        if let index = selectedLineIndex {
+            UIColor.green.setStroke()
+            let selectedLine = finishedLines[index]
+            stoke(selectedLine)
+        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -127,12 +143,82 @@ class DrawView: UIView{
     }
     
     @objc func doubleTap(_ gestureRecognizer: UIGestureRecognizer){
-        print("Double tap recognized!")
+        print("A double tap is recognized!")
         currentLines.removeAll()
         finishedLines.removeAll()
+        selectedLineIndex = nil
         setNeedsDisplay()
     }
     
+    func getSelectedLineIndex(_ point:CGPoint) -> Int? {
+        for(index, line) in finishedLines.enumerated(){
+            let begin = line.begin
+            let end = line.end
+            
+            for t in stride(from: CGFloat(0), to: 1.0, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                
+                if hypot(x-point.x, y-point.y) < 20.0{
+                    return index
+                }
+            }
+        }
+        return nil
+    }
+    
+    // ??? why the parameter is UIMenuController object?
+    @objc func deleteLine(_ sender: UIMenuController) {
+        if let index = selectedLineIndex {
+            finishedLines.remove(at: index)
+            selectedLineIndex = nil
+            
+            setNeedsDisplay()
+        }
+    }
+    
+    // ??? why the parameter is UIGestureRecognizer object?
+    @objc func tap(_ gestureRecognizer: UIGestureRecognizer) {
+        print("A tap is recognized!")
+        let location = gestureRecognizer.location(in: self)
+        selectedLineIndex = getSelectedLineIndex(location)
+        //print(selectedLineIndex!)
+        
+        let menu = UIMenuController.shared
+        
+        if selectedLineIndex != nil {
+            becomeFirstResponder()
+            
+            let deleteItem = UIMenuItem(title: "Delete",
+                                        action: #selector(DrawView.deleteLine(_:)))
+            menu.menuItems = [deleteItem]
+            
+            let targetRect = CGRect(x: location.x, y: location.y, width:2, height: 2)
+            menu.setTargetRect(targetRect, in: self)
+            menu.setMenuVisible(true, animated: true)
+            print("to delete")
+        } else {
+            menu.setMenuVisible(false, animated: true)
+        }
+        setNeedsDisplay()
+    }
+    
+    @objc func longPress(_ gestureRecognizer: UIGestureRecognizer) {
+        print("A long press is recognized!")
+        
+        if (gestureRecognizer.state == .began) {
+            let location = gestureRecognizer.location(in: self)
+            selectedLineIndex = getSelectedLineIndex(location)
+            
+            if (selectedLineIndex != nil) {
+                currentLines.removeAll()
+            }
+        } else if (gestureRecognizer.state == .ended) {
+            selectedLineIndex = nil
+        }
+        
+        setNeedsDisplay()
+    }
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
         //let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(doubleTap(_:)))
@@ -141,6 +227,16 @@ class DrawView: UIView{
         doubleTapRecognizer.numberOfTapsRequired = 2
         doubleTapRecognizer.delaysTouchesBegan = true
         addGestureRecognizer(doubleTapRecognizer)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self,
+                                                   action: #selector(DrawView.tap(_:)))
+        tapRecognizer.delaysTouchesBegan = true
+        tapRecognizer.require(toFail: doubleTapRecognizer)
+        addGestureRecognizer(tapRecognizer)
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self,
+                                                               action: #selector(DrawView.longPress))
+        addGestureRecognizer(longPressRecognizer)
         
     }
 }
